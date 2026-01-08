25b72b140922272fba688d5f9ead85916870cce0
Patricia Paredes <patriciakarel@hotmail.com>
2026-01-08 16:43:56 +0100

    feat: añadir clase Sale

--- Nivel1/Ejercicio1/Ejercicio1N1/src/model/Sale.java
+++ Nivel1/Ejercicio1/Ejercicio1N1/src/model/Sale.java
@@ -1,4 +1,34 @@
 package model;
 
+import exceptions.EmptySaleException;
+
+import java.util.ArrayList;
+
 public class Sale {
+    private ArrayList<Product> products;
+    private int totalPrice;
+
+    public Sale() {
+        this.products = new ArrayList<Product>();
+    }
+
+    public void setProducts(Product products) {
+        products.add(product);
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int calculateTotal() throws EmptySaleException{
+        totalPrice = 0;
+
+        if (products.isEmpty()){
+            throw new EmptySaleException("Para hacer una venta primero debes añadir productos");
+        }
+        for (Product product : products){
+            totalPrice += product.getPrice();
+        }
+        return totalPrice;
+    }
 }
